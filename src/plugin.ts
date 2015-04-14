084bbe84f596d2bff15b78c7ed5417d262fc06ea
Timo Weiß <info@timo-weiss.com>
2015-04-14 22:16:40 +0200

    add cookie validation/authentication

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,63 +5,103 @@ export interface IRegister {
 
 export default
 class Auth {
-    db: any;
-    bcrypt: any;
-    constructor(private mode: any) {
+    db:any;
+    bcrypt:any;
+    boom:any;
+
+    constructor(private mode:any) {
         this.register.attributes = {
             name: 'bemily-authentication',
             version: '0.1.0'
         };
         this.bcrypt = require('bcrypt');
+        this.boom = require('boom');
     }
 
     register:IRegister = (server, options, next) => {
         server.bind(this);
-        this._register(server, options);
-        server.register(require('hapi-auth-basic'), (err) => {
 
-            server.auth.strategy('simple', 'basic', this.mode, {validateFunc: this.validate});
+        server.register(require('hapi-auth-cookie'), (err) => {
+            if(err) {
+                this.errorInit(err);
+            }
+
+            server.auth.strategy('session', 'cookie', this.mode, {
+                password: 'secret',
+                ttl: 10000,
+                cookie: 'bemily_session',
+                isSecure: false
+            });
 
             server.dependency('bemily-database', (server, continueRegister) => {
                 this.db = server.plugins['bemily-database'];
                 continueRegister();
                 next();
+                this._register(server, options);
             });
         });
     };
 
     private _register(server, options) {
+
+        server.route({
+            method: 'GET',
+            path: '/logout',
+            handler: (request, reply) => {
+                request.auth.session.clear();
+                reply('logged out');
+            }
+        });
+
         server.route({
             method: 'POST',
-            path: '/auth',
-            handler: (request, reply) => {
-                reply();
+            path: '/login',
+            config: {
+                auth: {
+                    mode: 'try',
+                    strategy: 'session'
+                },
+                handler: this.login
             }
-        })
+        });
 
     }
 
-    validate = (username, password, callback) => {
+    login (request, reply) {
+        if (request.auth.isAuthenticated) {
+            return reply('already authenticated');
+        }
+        if(typeof request.payload === 'string') {
+            request.payload = JSON.parse(request.payload)
+        }
 
-        this.db.getUserLogin(username, (err, user) => {
-            if(!user || !user.length) {
-                return callback(null, false);
-            }
+        if (!request.payload.username || !request.payload.password) {
+            return reply(this.boom.badRequest('Missing username or password'));
+        }
+        else {
+            this.db.getUserLogin(request.payload.username, (err, user) => {
+                if (!user || !user.length) {
+                    // TODO check LDAP
+                    return reply(this.boom.unauthorized('NEEDS LDAP CHECK'));
+                }
+                if (request.payload.password === user[0].value.password) {
+                    reply(user[0]);
+                    request.auth.session.set(user[0]);
+                } else {
+                    reply(this.boom.unauthorized('Wrong username or password'));
+                }
 
-            if(password === user[0].value.password) {
-                callback(err, true, user[0]);
-            }
+                //this.bcrypt.compare(password, user[0].value.password, function (err, isValid) {
+                //    reply(err, isValid, user[0]);
+                //});
+            });
 
-            //this.bcrypt.compare(password, user[0].value.password, function (err, isValid) {
-            //    callback(err, isValid, user[0]);
-            //});
-        });
+        }
+    }
 
-    };
-
-    errorInit(error) {
-        if (error) {
-            console.log('Error: Failed to load plugin:', error);
+    errorInit(err) {
+        if (err) {
+            console.log('Error: Failed to load plugin:', err);
         }
     }
 }
